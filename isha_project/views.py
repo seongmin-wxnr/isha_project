6ca21b4172f83ac8f94b06d9635c2adf13d48c8a
seongmin-wxnr <yuilove9105@gmail.com>
2026-04-14 02:04:03 +0900

    clear commit

---
+++ isha_project/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+# 페이지 렌더링 뷰 함수
+@require_http_methods(["GET"])
+def isha_main_rendering(request):
+    return render(request, "main/isha_main.html")
+
+@require_http_methods(["GET"])
+def isha_legal_rendering(request):
+    return render(request, "main/isha_legal.html")
+
+@require_http_methods(["GET"])
+def isha_intro_rendering(request):
+    return render(request, "main/isha_member.html")
+
+@require_http_methods(["GET"])
+def isha_news_rendering(request):
+    return render(request, "main/isha_news.html")
+
+@require_http_methods(["GET"])
+def isha_schedule_rendering(request):
+    return render(request, "main/isha_schedule.html")
+
+# status -> 방송 상태 및 실시간 렌더
+@require_http_methods(['GET'])
+def isha_status_main(request):
+    return render(request, "main/status/isha_status_main.html")
+
